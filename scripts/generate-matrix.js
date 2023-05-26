79c094c8131fe1699921c3f9fbf76718173db140
Johannes Przymusinski <j.przymusinski@die-lobby.de>
2023-05-26 17:18:10 +0200

    task: load supported php versions from shopware-static-data

--- scripts/generate-matrix.js
+++ scripts/generate-matrix.js
@@ -6,26 +6,54 @@ const FLAVORS = [
 ];
 
 /**
- * @todo: load from https://github.com/FriendsOfShopware/shopware-static-data/pull/1
+ * @todo change url when https://github.com/FriendsOfShopware/shopware-static-data/pull/1 is merged.
  */
-const PHP_VERSIONS = [
-    "8.0",
-    "8.1",
-    "8.2",
-];
+const loadShopwarePHPVersions = async () => {
+    const response = await fetch("https://raw.githubusercontent.com/FriendsOfShopware/shopware-static-data/main/data/all-supported-php-versions-by-shopware-version.json")
+    return await response.json();
+}
 
-const getTemplate = (version) => {
+const parseMinorVersion = (version) => {
     const parts = version.match(/v[0.9]*.\.([0-9]*)\..*/);
     if (parts.length < 2) {
         throw new Error("Unexpected version format: " + version);
     }
 
-    const minor = parts[1];
-    if (parseInt(minor) <= 4) {
+    return parseInt(parts[1], 10);
+}
+
+const getTemplate = (version) => {
+    const minor = parseMinorVersion(version);
+
+    // For everything below 6.5, we need the shopware/production template.
+    if (minor <= 4) {
         return "https://github.com/shopware/production";
     }
 
     return "https://github.com/shopware/platform";
+}
+
+/**
+ * Helper to find the supported PHP versions in the static-dump. 
+ * Needed because some (older) shopware tags have an v before the composer version.
+ */
+const findSupportedPhpVersion = (supportedPhpVersions, shopwareVersion) => {
+    const parts = shopwareVersion.match(/^v?(.*)/);
+    if (parts.length < 2) {
+        throw new Error(`Something went wrong, extracting the version from ${shopwareVersion}. Got ${parts} from the regex.`)
+    }
+    const version = parts[1];
+
+    if (supportedPhpVersions[version]) {
+        return supportedPhpVersions[version];
+    }
+
+    if (supportedPhpVersions['v' + version]) {
+        return supportedPhpVersions['v' + version];
+    }
+
+    // Shopware Version not found. Can happen when e.g. RCs are not published on Packagist.
+    return undefined;
 }
 
 async function main() {
@@ -34,13 +62,9 @@ async function main() {
 
     const versions = tags.map((tag) => {
         const version = tag.ref.replace("refs/tags/", "");
-        const parts = version.match(/v[0.9]*.\.([0-9]*)\..*/);
-        if (parts.length < 2) {
-            return;
-        }
-
-        const minor = parts[1];
-        // skip 
+        const minor = parseMinorVersion(version);
+        
+        // We only want to support versions >= 6.3 to reduce build time
         if (parseInt(minor) <= 3) {
             return;
         }
@@ -48,24 +72,31 @@ async function main() {
         return version;
     }).filter(version => !!version);
 
-    let matrix = versions.map((version) => {
-        const template = getTemplate(version);
+    const shopwarePhpVersions = await loadShopwarePHPVersions();
 
-        const matrix = [];
+    let matrix = versions
+        .map((version) => {
+            const template = getTemplate(version);
 
-        for (let flavor in FLAVORS) {
-            for (let phpVersion in PHP_VERSIONS) {
-                matrix.push({
-                    "shopware-version": version,
-                    "php-version": phpVersion,
-                    "flavour": flavor,
-                    "template": template,
-                })
-            }
-        }
+            const matrix = [];
 
-        return matrix;
-    })
+
+            const supportedPhpVersions = findSupportedPhpVersion(shopwarePhpVersions, version);
+
+            FLAVORS.forEach((flavor) => {
+                supportedPhpVersions.forEach((phpVersion) => {
+                    matrix.push({
+                        "shopware-version": version,
+                        "php-version": phpVersion,
+                        "flavour": flavor,
+                        "template": template,
+                    })
+                });
+            });
+
+            return matrix;
+        })
+        .flat();
 
     console.log(JSON.stringify({
         include: matrix,
